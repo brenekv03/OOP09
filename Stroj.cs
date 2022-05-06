6767c57ebbe9048b2c76ec11c4de5b1503aa8c85
brenekv.03 <brenekv.03@A321-15>
2022-05-06 09:25:25 +0200

    class stroj

---
+++ Stroj.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP09
+{
+    class Stroj
+    {
+        private string evidencniCislo;
+        private DateTime posledniServis = new DateTime();
+        public string EvidencniCislo
+        {
+            get
+            {
+                return evidencniCislo;
+            }
+            set
+            {
+                string s = value;
+                int i = 0;
+                while(i<=1)
+                {
+                    if (!char.IsLetter(s[i]))
+                    {
+                        string substr = s.Substring(i);
+                        s = s.Replace(s[i], 'A');
+                    }
+                    else ++i;
+                }
+                evidencniCislo = s;
+            }
+        }
+        public DateTime UvedenDoProvozu { get; }
+        public Stroj(string evidencniCislo, DateTime uvedenDoProvozu)
+        {
+            EvidencniCislo = evidencniCislo;
+            UvedenDoProvozu = uvedenDoProvozu;
+        }
+        public void ProvedServis()
+        {
+            if (DateTime.Today > posledniServis.AddDays(42))
+            {
+                posledniServis = DateTime.Today;
+            }
+        }
+        public override string ToString()
+        {
+            string s = "\nEvidenční číslo: " + EvidencniCislo +
+                "\nUvedení do provozu: " + UvedenDoProvozu.ToShortDateString() +
+                "\nDatum posledního servisu: " + posledniServis.ToShortDateString(); 
+            return base.ToString()+s; 
+        }
+    }
+}
